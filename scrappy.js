c292129925d7c46b9d3892f6ffa4ef8a05e96978
yianyue <yianyue@gmail.com>
2015-08-11 15:40:42 -0400

    add error to console log

--- scrappy.js
+++ scrappy.js
@@ -9,19 +9,21 @@ module.exports = function(fileName){
 
   request(webpage, function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      // Convert the body of the response into a cheerio object for jquery
       var $ = cheerio.load(body);
+      // Create a write stream on a file
       var file = fs.createWriteStream(fileName);
-
+      // loop through each row of the table to get the data
       $('table').find('tr').each(function(i, elem){      
         var permission = $(this).find('td:first-child').text();
         var url = website + $(this).find('a').attr('href');
         var type = $(this).find('td').last().text().split('.')[1];
-
+      // only write to file if there's a file type (not a directory)
         if (type){file.write(permission + ',' + url + ',' + type + '\n');}
       });
 
     } else {
-      console.log('FAIL!');
+      console.log(error);
     }
   });
 

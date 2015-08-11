72f97800973d61c42ed2ae63667331b29e5dcb3c
yianyue <yianyue@gmail.com>
2015-08-11 15:15:32 -0400

    working

--- scrappy.js
+++ scrappy.js
@@ -7,16 +7,20 @@ var fs = require('fs');
 
 var webpage = 'http://substack.net/images/';
 var website = 'http://substack.net';
+var fileName = 'images.csv';
 
 request(webpage, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    var data = [];
+    // var data = [];
     var $ = cheerio.load(body);
+    var file = fs.createWriteStream(fileName);
 
-    $('table').find('tr').each(function(i, elem){
+    $('table').find('tr').each(function(i, elem){      
       var permission = $(this).find('td:first-child').text();
       var url = website + $(this).find('a').attr('href');
       var type = $(this).find('td').last().text().split('.')[1];
+
+      if (type){file.write(permission + ',' + url + ',' + type + '\n');}
     });
 
   } else {
